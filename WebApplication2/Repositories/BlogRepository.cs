fa22c2d81324fddd506875c832d384901424a686
MOANICKEL\maikelv <maikelv@moanickel.com.cu>
2018-06-08 15:50:09 -0400

    Add project files.

---
+++ WebApplication2/Repositories/BlogRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.SQLite;
+
+namespace WebApplication2.Repositories
+{
+    public class BlogRepository : Repository<Blog>, IBlogRepository
+    {
+        public BlogRepository(BloggingContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
